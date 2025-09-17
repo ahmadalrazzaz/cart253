d5695b2477c91b60693b5ee0c4029368b1b68b78
Ahmad AlRazzaz <ahmadalrazzaz7@gmail.com>
2025-09-17 19:01:53 -0400

    Pasted code and made Mr Furious more red over time

---
+++ variables-challenge/js/script.js
@@ -0,0 +1,49 @@
+/*
+
+Ahmad AlRazzaz
+Variables Challenge 
+CART 253 - September 17
+
+ */
+
+"use strict";
+
+// Our friend Mr. Furious
+let mrFurious = {
+  // Position and size
+  x: 200,
+  y: 200,
+  size: 100,
+  // Colour
+  fill: {
+    r: 255,
+    g: 225,
+    b: 225,
+  },
+};
+
+
+/**
+ * Create the canvas
+ */
+function setup() {
+  createCanvas(400, 400);
+}
+
+/**
+ * Draw (and update) Mr. Furious
+ */
+function draw() {
+  background(160, 180, 200);
+
+  // Draw Mr. Furious as a coloured circle
+  push();
+  noStroke();
+  fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+  ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  pop();
+
+  // Slowly make Mr. Furious more red
+  mrFurious.fill.g -= 0.3;
+  mrFurious.fill.b -= 0.3;
+}
