d90e43c73aa301401d83c38393ff8aa526b3c163
Ahmad AlRazzaz <ahmadalrazzaz7@gmail.com>
2025-09-24 19:17:43 -0400

    Added extra force when puck is contacted again (even if already moving)

--- conditionals-challenge/js/script.js
+++ conditionals-challenge/js/script.js
@@ -13,14 +13,14 @@ const puck = {
   fill: "#ff0000",
   isMoving: false,
   speed: 5,
-  movementAngle: 0
+  movementAngle: 0,
 };
 
 const user = {
   x: undefined, // will be mouseX
   y: undefined, // will be mouseY
   size: 75,
-  fill: "#000000"
+  fill: "#000000",
 };
 
 /**
@@ -35,32 +35,36 @@ function setup() {
  */
 function draw() {
   background("#aaaaaa");
-  
+
   // Move user circle
   moveUser();
-  
+
   // Draw the user and puck
   drawUser();
   drawPuck();
 
   const d = dist(user.x, user.y, puck.x, puck.y);
+  const overlap = d < user.size / 2 + puck.size / 2;
 
-  const overlap = (d < user.size/2 + puck.size/2);
   if (overlap) {
     // Finding angle of contact
     puck.movementAngle = atan2(puck.y - user.y, puck.x - user.x);
+
+    if(puck.isMoving) {
+        puck.speed += 2;
+    }
+
     puck.isMoving = true;
   }
 
-    if (puck.isMoving) {
-        movePuck();
-        // Reset puck when speed reaches 0
-        if (puck.speed <= 0) {
-            puck.isMoving = false;
-            puck.speed = 5;
-        }
+  if (puck.isMoving) {
+    movePuck();
+    // Reset puck when speed reaches 0
+    if (puck.speed <= 0) {
+      puck.isMoving = false;
+      puck.speed = 5;
     }
-
+  }
 }
 
 /**
@@ -94,17 +98,15 @@ function drawPuck() {
 }
 
 function movePuck() {
-    
-    puck.x += cos(puck.movementAngle) * puck.speed;
-    puck.y += sin(puck.movementAngle) * puck.speed;
-    
-    puck.speed -= 0.03; // Decrease speed over time
+  puck.x += cos(puck.movementAngle) * puck.speed;
+  puck.y += sin(puck.movementAngle) * puck.speed;
 
-    if (puck.x > width || puck.x < 0) {
-        puck.movementAngle = PI - puck.movementAngle;
-    }
-    if (puck.y > height || puck.y < 0) {
-        puck.movementAngle = -puck.movementAngle;
-    }
+  puck.speed -= 0.04; // Decrease speed over time
 
+  if (puck.x > width || puck.x < 0) {
+    puck.movementAngle = PI - puck.movementAngle;
+  }
+  if (puck.y > height || puck.y < 0) {
+    puck.movementAngle = -puck.movementAngle;
+  }
 }
